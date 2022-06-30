2ae808a5ff84a6b93e7ec590b6c4ccf953259bec
Chris Young <cyoung43@byu.edu>
2022-06-30 09:33:11 -0600

    testing commit messgaes

--- dags/dbt/models/dw/shot_logs.sql
+++ dags/dbt/models/dw/shot_logs.sql
@@ -9,14 +9,14 @@ select
     p.parse:MATCHUP::varchar as MATCHUP,
     p.parse:LOCATION::varchar as LOCATION,
     p.parse:player_name::varchar as PLAYER_NAME, --@col Player Name. Name of the player taking the shot
-    p.parse:player_id::int as PLAYER_ID,
-    p.parse:SHOT_NUMBER::int as SHOT_NUMBER,
+    p.parse:player_id::int as PLAYER_ID, --@col ID of the player taking the shot
+    p.parse:SHOT_NUMBER::int as SHOT_NUMBER, --@col Shot number the player has taken during the game
     p.parse:PERIOD::int as QUARTER,
     p.parse:GAME_CLOCK::varchar as GAME_CLOCK,
-    p.parse:SHOT_CLOCK::numeric(5, 1) as SHOT_CLOCK,
-    p.parse:DRIBBLES::int as DRIBBLES,
-    p.parse:TOUCH_TIME::numeric(5, 1) as TOUCH_TIME,
-    p.parse:SHOT_DIST::numeric(5, 1) as SHOT_DISTANCE,
+    p.parse:SHOT_CLOCK::numeric(5, 1) as SHOT_CLOCK,--@col How much time available on the shot clock when player took shot
+    p.parse:DRIBBLES::int as DRIBBLES, --@col How many dribbles player took before shot [int]
+    p.parse:TOUCH_TIME::numeric(5, 1) as TOUCH_TIME, --@col How long the player had the ball before shot [int]
+    p.parse:SHOT_DIST::numeric(5, 1) as SHOT_DISTANCE, --@col Distance of the shot [double]
     p.parse:PTS_TYPE::int as POINTS_TYPE,
     p.parse:SHOT_RESULT::varchar as SHOT_RESULT,
     p.parse:CLOSEST_DEFENDER::varchar as CLOSEST_DEFENDER,
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # This file will add the necessary precommit files to enable the autogeneration of the DBT data dictionary
 
-mkdir .githooks || echo "Directory .githooks already exists"
+# How to create symlink between directories
+# mkdir .githooks || echo "Directory .githooks already exists"
 
 directory=".git/hooks"
 # [ -d $directory ] && rm -r $directory/* || mkdir $directory && echo "created directory"
@@ -12,11 +13,15 @@ else
     echo "created directory"
 fi
 
-ln -s .githooks .git/hooks/ && echo "created link" || echo "failed to link"
+# Uncomment next line if able to figure out symlink
+# ln -s .githooks .git/hooks/ && echo "created link" || echo "failed to link"
 
 pip3 install pyyaml
 
-cd .githooks && wget https://raw.githubusercontent.com/cyoung43/airflow-pipeline/master/.githooks/generate_dbt_data_dictionary.py
+cd .git/hooks && wget https://raw.githubusercontent.com/cyoung43/airflow-pipeline/master/.githooks/generate_dbt_data_dictionary.py
 wget https://raw.githubusercontent.com/cyoung43/airflow-pipeline/master/.githooks/pre-commit
+
+chmod 777 pre-commit
+chmod 777 generate_dbt_data_dictionary.py
 
 echo "Setup complete"
